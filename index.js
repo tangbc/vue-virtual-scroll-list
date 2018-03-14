16b688e1f05f5d7605bbaecdf02a5c7265d1ce47
TANG <40519806@qq.com>
2018-03-14 17:15:27 +0800

    Use zone-hit instead of filter.

--- index.js
+++ index.js
@@ -314,9 +314,11 @@
                 delta.paddingBottom = paddingBottom
                 delta.offsetAll = allHeight - this.size * this.remain
 
-                return slots.filter(function (slot, index) {
-                    return index >= delta.start && index <= delta.end
-                })
+                var targets = []
+                for (var i = delta.start; i <= delta.end; i++) {
+                    targets.push(slots[i])
+                }
+                return targets
             }
         },
 
