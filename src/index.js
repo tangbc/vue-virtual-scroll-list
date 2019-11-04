5c8f5f203a3098104f8b791b16d25bfe08b437e0
杨阳 <1024920618@qq.com>
2019-11-04 18:33:43 +0800

    Update index.js
    change $scopedSlots.default to $scopedSlots.item

--- src/index.js
+++ src/index.js
@@ -508,7 +508,7 @@
         const slots = this.$slots.default || []
 
         // item-mode shoud judge from items prop.
-        if (this.item || this.$scopedSlots.default) {
+        if (this.item || this.$scopedSlots.item) {
           delta.total = this.itemcount
           if (delta.keeps > delta.total) {
             delta.end = delta.total - 1
@@ -544,8 +544,8 @@
         const renders = []
         for (let i = delta.start; i < delta.total && i <= Math.ceil(delta.end); i++) {
           let slot = null
-          if (this.$scopedSlots.default) {
-            slot = this.$scopedSlots.default(i)
+          if (this.$scopedSlots.item) {
+            slot = this.$scopedSlots.item(i)
           } else if (this.item) {
             slot = h(this.item, this.itemprops(i))
           } else {
