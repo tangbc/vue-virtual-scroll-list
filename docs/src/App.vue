4ac14eef7bdb537c576f5433ce059676193a5287
TANG <40519806@qq.com>
2020-04-06 12:25:14 +0800

    Add vue router

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,28 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ docs/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
