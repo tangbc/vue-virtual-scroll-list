8020ae02bdfc301c640a173c29888cf58233b1db
TANG <40519806@qq.com>
2020-04-11 21:05:43 +0800

    Add index page

--- example/src/views/Index/Main.vue
+++ example/src/views/Index/Main.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="index">
     <github-corner />
+
+    <h2 class="name">vue-virtual-scroll-list</h2>
+
+    <div class="head">
+      <img src="https://nodei.co/npm/vue-virtual-scroll-list.png?downloads=true&downloadRank=true&stars=true">
+    </div>
+
+    <h3 class="title">What's news in v2.0</h3>
+    <ul>
+      <li>Support header slot, footer slot.</li>
+      <li>Code structure is more reasonable and good maintainability.</li>
+      <li>Abandoned v-for mode and variable mode, all condition unify into one mode.</li>
+      <li>Don't have to care about each item size, in sometimes it's difficult to know that.</li>
+    </ul>
+
+    <h3 class="title">Todos</h3>
+    <ul>
+      <li>Improve calculate offset efficient.</li>
+      <li>Support page scroll, table layout.</li>
+      <li>......</li>
+    </ul>
   </div>
 </template>
 
@@ -9,8 +30,19 @@ export default {
   name: 'index',
 
   data() {
-    return {
-    }
+    return {}
   }
 }
 </script>
+
+<style lang="less" scoped>
+.name {
+  margin: 1em 0;
+}
+.head {
+  margin-bottom: 2em;
+}
+.title {
+  margin: 1em 0;
+}
+</style>
