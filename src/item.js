46f860083a89b6e5946cddf91e6ad3e84a6155ff
TANG <40519806@qq.com>
2021-09-06 18:50:30 +0800

    Eslint fix

--- src/item.js
+++ src/item.js
@@ -51,12 +51,12 @@ export const Item = Vue.component('virtual-list-item', {
   props: ItemProps,
 
   render (h) {
-    const { tag, component, extraProps = {}, index, source, scopedSlots = {}, uniqueKey, slotComponent } = this;
+    const { tag, component, extraProps = {}, index, source, scopedSlots = {}, uniqueKey, slotComponent } = this
     const props = {
-        ...extraProps,
-        source,
-        index,
-    };
+      ...extraProps,
+      source,
+      index
+    }
 
     return h(tag, {
       key: uniqueKey,
